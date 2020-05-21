a5dc111c9205709e210a8b5f9ba6e25e34e2744d
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-22 00:36:11 +0200

    Add tests for helpers

---
+++ helpers/helpers_test.go
@@ -0,0 +1,32 @@
+package helpers
+
+import "testing"
+
+func TestConvertDestructAfterOpeningToBool(t *testing.T) {
+	_, err := ConvertSelfDestructToInt("1 hour")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ConvertSelfDestructToInt("100 hours")
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestConvertSelfDestructToInt(t *testing.T) {
+	_, err := ConvertDestructAfterOpeningToBool("no")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ConvertDestructAfterOpeningToBool("yes")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ConvertDestructAfterOpeningToBool("oops")
+	if err == nil {
+		t.Fail()
+	}
+}
