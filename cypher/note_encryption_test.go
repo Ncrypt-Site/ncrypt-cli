879ca4d0bce1d216f49f2b11aab4e0c7296fe4df
Farshad Nematdoust <farshad@nematdoust.com>
2020-05-24 19:36:28 +0200

    Update note encryption test

--- cypher/note_encryption_test.go
+++ cypher/note_encryption_test.go
@@ -1,18 +1,21 @@
 package cypher
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestEncryptMessage(t *testing.T) {
 	note := []byte("It does not do well to dwell on dreams and forget to live")
 	key := []byte("3Go2iV@Rb$y*OdIMP3anI89@0i%o!e%w")
+	iv := []byte("#83o7D7PvEw2YJx9")
 
-	_, err := EncryptNote(note, key)
+	_, err := EncryptNote(note, key, iv)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	invalidKey := []byte("I'm an invalid key, ha ha ...")
-	_, err = EncryptNote(note, invalidKey)
+	_, err = EncryptNote(note, invalidKey, iv)
 	if err == nil {
 		t.Fail()
 	}
