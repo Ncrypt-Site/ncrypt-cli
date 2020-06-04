6de950b1a5984d8e2f01537131bb02c67bdc9e07
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-04 18:57:13 +0200

    Add note model

--- models/note.go
+++ models/note.go
@@ -1,5 +1,11 @@
 package models
 
+type Note struct {
+	Note                 []byte
+	SelfDestruct         int
+	DestructAfterOpening bool
+}
+
 type EncryptedNote struct {
 	Note string
 	Key  string
