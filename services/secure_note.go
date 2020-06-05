49c8cce0b44470e0bcb2f4f3a36d615dd26e63b2
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-05 22:35:41 +0200

    Add secure note process (CreateNote func) This will encapsulate some part of code from main.go

---
+++ services/secure_note.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"encoding/json"
+	"errors"
+	"ncrypt-cli/models"
+)
+
+func CreateNote(h HttpService, n models.Note) (models.NoteCreatedResponse, string, error) {
+	encryptedNote, err := createSecureNote(n.Note)
+	if err != nil {
+		return models.NoteCreatedResponse{}, "", errors.New("an error occurred, please try again")
+	}
+
+	model := models.SecureMessageRequest{
+		Note:                 encryptedNote.Note,
+		SelfDestruct:         n.SelfDestruct,
+		DestructAfterOpening: n.DestructAfterOpening,
+	}
+
+	resp, err := h.SendRequest(model)
+	if err != nil {
+		return models.NoteCreatedResponse{}, "", err
+	}
+
+	r := models.NoteCreatedResponse{}
+	err = json.Unmarshal(resp, &r)
+	if err != nil {
+		return models.NoteCreatedResponse{}, "", err
+	}
+
+	return r, encryptedNote.Key, nil
+}
