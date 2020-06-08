9e2cee64fc52aaadf7649657a47462598bbefca5
Farshad Nematdoust <farshad@nematdoust.com>
2020-06-08 20:24:34 +0200

    Add comments to note.go

--- models/note.go
+++ models/note.go
@@ -1,11 +1,13 @@
 package models
 
+//Note model for http request
 type Note struct {
 	Note                 []byte
 	SelfDestruct         int
 	DestructAfterOpening bool
 }
 
+//EncryptedNote model for returning data to the user
 type EncryptedNote struct {
 	Note string
 	Key  string
